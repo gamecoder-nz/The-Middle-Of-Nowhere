69df73ae2a0ced4e7523dad256431c3f6d18b8b8
gamecoder-nz <42922238+gamecoder-nz@users.noreply.github.com>
2021-07-08 08:15:43 +1200

    Added RigidBodyComponent

--- Components.hpp
+++ Components.hpp
@@ -2,9 +2,17 @@
 #define COMPONENTS_HPP
 
 #include <SFML/Graphics/Vertex.hpp>
+#include <SFML/Graphics/Rect.hpp>
 
 struct TransformComponent
 {
 	sf::Vector2f Position;
+};
+
+struct RigidBodyComponent
+{
+	sf::Vector2f Velocity;
+	sf::Vector2f HitboxOffset;
+	sf::FloatRect Hitbox;
 };
 #endif
